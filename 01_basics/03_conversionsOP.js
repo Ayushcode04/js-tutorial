f91009aa408f12bc8bf5f5bddae7084c49becc84
Ayushcode04 <124444459+Ayushcode04@users.noreply.github.com>
2024-12-25 20:42:48 +0000

    Conversionfile

---
+++ 01_basics/03_conversionsOP.js
@@ -0,0 +1,29 @@
+let score ="37"
+
+console.log(typeof score);
+
+let valueINumber =Number(score)
+console.log(typeof valueINumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedin = "A"
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+ console.log(booleanIsLoggedIn);
+
+ // 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+
