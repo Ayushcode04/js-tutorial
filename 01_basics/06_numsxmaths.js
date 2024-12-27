8105bbe0e26731f130aaddb7b169d7851b1f0694
Ayushcode04 <124444459+Ayushcode04@users.noreply.github.com>
2024-12-27 20:34:47 +0000

    completed math

--- 01_basics/06_numsxmaths.js
+++ 01_basics/06_numsxmaths.js
@@ -19,6 +19,19 @@ const hundreds = 1000000
 
 //=++++++++++++++++++++++++++MAths+++++++++++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(4));
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.ceil(4.2));
 
+// min,max,floor,random
+// console.log(Math.max(4, 3, 6, 8));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
