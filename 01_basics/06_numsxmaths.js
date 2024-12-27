1ed4d305afc25d8590d59acdcc983110fee4e506
Ayushcode04 <124444459+Ayushcode04@users.noreply.github.com>
2024-12-27 17:41:50 +0000

    partially done

---
+++ 01_basics/06_numsxmaths.js
@@ -0,0 +1,24 @@
+const score = 400
+// console.log(score);
+
+
+
+const balance = new Number(100)
+// console.log(balance);
+ 
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const othernumber =123.9899
+
+// console.log(othernumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//=++++++++++++++++++++++++++MAths+++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(4));
+
