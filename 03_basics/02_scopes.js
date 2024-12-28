325646a0c8e1e5c78d6ada2a88748b3da1612070
Ayushcode04 <124444459+Ayushcode04@users.noreply.github.com>
2024-12-28 17:04:43 +0000

    scopes in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,56 @@
+// var c =300
+let a=300
+ if (true){
+    let a=10
+    const b=20
+    // console.log("inn",a);
+} 
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one() {
+    const username ="Ayush"
+
+    function two() {
+        const website = "YouTube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+
+    
+}
+one()
+
+if (true){
+    const username = "Ayush"
+    if (username === "Ayush"){
+        const website =" Youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+//+++++++++++interesting++++++++++++++
+
+
+console.log(addone(5));
+
+function addone(num) {
+    return num+1
+}
+
+addTwo(5)
+const addTwo = function (num) {
+    return num+2
+}
+
+
