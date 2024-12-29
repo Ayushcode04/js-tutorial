b97763a020f32b0a1bb615a23f9e763472b45b0e
Ayushcode04 <124444459+Ayushcode04@users.noreply.github.com>
2024-12-29 20:59:49 +0000

    one .js tut

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,48 @@
+//if
+const isuserLoggedin = true
+if (2 !=2) {
+    // console.log("executed");
+    
+}else{
+    // console.log("noo");
+    
+}
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+const balance =1000
+// if (balance>200)console.log("test"),,console.log("test2")//not to include to print in same
+
+//  if (balance<500) {
+//     console.log("less than");
+    
+//  }else if(balance<750){
+//     console.log("less than 750");
+    
+//  }
+//  else{
+//     console.log();
+    
+//  }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+
+if (userLoggedIn && debitCard ) {
+    console.log(("ALLOW TO BUY"));
+}
+
+if (loggedInFromEmail||loggedInFromGoogle) {
+    console.log("User logged in");   
+}
+
+
+
