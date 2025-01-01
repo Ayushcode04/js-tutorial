49ec1387c673b2035d00dd2eb645f4fbfbe6821a
Ayushcode04 <124444459+Ayushcode04@users.noreply.github.com>
2025-01-01 16:47:47 +0000

    for of for in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index = 2
+while (index <=10) {
+    // console.log(`value of index is ${index}`);
+    index =index+2
+}
+
+let myArr =[ "spiderman","supe","butcher"]
+
+let arr = 0
+while (arr < myArr.length) {
+    console.log(`value ${myArr[arr]}`);
+    arr+=1
+}
+
+let score =1
+
+do {
+    console.log(`Score is ${score}`);
+    score ++
+} while (score<=10);
